8c5187bb59afbfd6a0152473d394c7fe355ab294
Nikita shek <sheikinnikitka@yandex.ru>
2025-10-15 07:51:00 +0300

    решение

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+int main(int argc,char *argv[]){
+    float a[10];
+    a[0]=atof(argv[1]);
+    for(int i =0; i<=9; i++){ 
+        a[i] = atof(argv[i+1]);
+    }
+
+    float c = 0;
+    for(int i = 0; i<=9; i++){
+        c+=a[i];
+    }
+    printf("%.2f", c/10);
 }
--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+int main(int argc, char* argv[]){
+float a[10];
+for(int i = 0; i<=10; i++){
+    a[i]=atof(argv[i+1]);
+}
+for(int i = 9; i>=0; i--){
+    printf("%.1f ",a[i]);
+}
 }
