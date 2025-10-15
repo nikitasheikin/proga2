abe477511a962b0192c8ee27a49eec962b7d8bf2
Nikita shek <sheikinnikitka@yandex.ru>
2025-10-15 07:56:40 +0300

    решение

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,14 +2,13 @@
 #include<stdlib.h>
 
 int main(int argc,char *argv[]){
-    float a[10];
-    a[0]=atof(argv[1]);
-    for(int i =0; i<=9; i++){ 
-        a[i] = atof(argv[i+1]);
+double a[10] = {};
+    for(int i =0; i<10; i++){ 
+        scanf("%lf",&a[i]);
     }
 
     float c = 0;
-    for(int i = 0; i<=9; i++){
+    for(int i = 0; i<10; i++){
         c+=a[i];
     }
     printf("%.2f", c/10);
